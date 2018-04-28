69bc4394476f126a33d02a021c7ab56ddd2541ac
Yahya Alhaddad <yahyaalhaddad@yahyas-mbp.home>
2018-04-28 09:49:25 -0400

    Friend Finder

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friends = [
+  {
+    name: "Mary",
+    photo: "https://www.cepolina.com/photo/people/faces/b/people-girl-look-up.jpg",
+    scores: ["1", "3", "5", "3","1", "3", "5", "3","1", "3"]
+  },
+  {
+    name: "Dave",
+    photo: "https://kovla.com/blog/wp-content/uploads/2016/06/BlogPostImage_33.jpg",
+    scores: ["1", "3", "1", "3","5", "3", "5", "4","1", "3"]
+  }
+];
+
+module.exports = friends;
